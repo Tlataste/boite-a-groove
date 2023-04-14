d2fc3f5e0d6b0ecbfc3c0f756823790a297aec36
Matteofirrone <firrone.matteo@gmail.com>
2023-04-14 21:38:03 +0200

    Added components & front routes

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import RegisterPage from "./RegisterPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<p>This is the home page</p>} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
+    );
+  }
+}
--- frontend/urls.py
+++ frontend/urls.py
@@ -4,5 +4,6 @@ from .views import index
 # Here, dispatch the URLs to the correct app
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('register', index)
 ]
