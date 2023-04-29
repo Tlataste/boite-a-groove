1997c28c277af5179cfa868f17f43f263f013de7
Matteofirrone <firrone.matteo@gmail.com>
2023-04-29 18:55:37 +0200

    Created home page

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,5 +1,20 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Menu from "./Menu";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        background: "linear-gradient(to right, #F59225, #F8431D)",
+      }}
+    >
+      <Menu />
+    </Box>
+  );
 }
