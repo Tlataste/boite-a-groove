f4bb1e2f790f58b6bce4024611eaf93be06ca66f
TUCHAGUES <axel.tuchagues@orange.fr>
2023-05-15 11:24:06 +0200

    Ajout des classes dans models

--- gestion_boites/models.py
+++ gestion_boites/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Boites(models.Model):
+    id_boite = models.AutoField(primary_key=True)
+    nom_boite = models.CharField(max_length=50, unique=True)
+    url_image = models.CharField(max_length=150)
+    description_boite = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    location_lat = models.CharField(max_length=50, unique=True)
+    location_long = models.CharField(max_length=50, unique=True)
+    client = models.CharField(max_length=50, unique=True)
+
+
+class Song(models.Model):
+    id_song = models.AutoField(primary_key=True)
+    name_song = models.CharField(max_length=50, unique=True)
+    name_artist = models.CharField(max_length=50, unique=True)
+    url_song = models.CharField(max_length=150)
+    url_image = models.CharField(max_length=150)
+    n_deposits = models.IntegerField(default=0)
+
+
