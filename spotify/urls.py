43f720dba5ec52157186556b150dc9836640fc41
Anthony De Sousa <hds.anthony@gmail.com>
2023-05-16 01:14:46 +0200

    Changed URLs for simplicity

--- spotify/urls.py
+++ spotify/urls.py
@@ -3,8 +3,8 @@ from .views import AuthURL, spotify_callback, IsAuthenticated, GetRecentlyPlayed
 
 
 urlpatterns = [
-    path('get-auth-url', AuthURL.as_view()),
+    path('auth-redirection', AuthURL.as_view()),
     path('redirect', spotify_callback),
     path('is-authenticated', IsAuthenticated.as_view()),
-    path('get-recently-played-tracks', GetRecentlyPlayedTracks.as_view())
+    path('recent-tracks', GetRecentlyPlayedTracks.as_view())
 ]
