8aff62d200ce7c76bd5efafbb3c4a4341b4536d1
LOURDEL <emilie.lourdel@orange.fr>
2023-05-16 12:15:20 +0200

    maj de song lors d'un depot

--- gestion_boites/views.py
+++ gestion_boites/views.py
@@ -17,6 +17,26 @@ def boite_detail(request, url):
     print(chansons)
     return render(request, 'frontend/affichage_boite.html', {'boite': boite, 'chansons': chansons})
 
-# def boite_detail(request, url):
-#     boite = get_object_or_404(Box, url_box=url)
-#     return HttpResponse("Bienvenue dans la boite à son de l'arrêt " + boite.nom_boite + ' (réseau ' + boite.client +')')
+
+def ajouter_chanson(request, url, nom_chanson='Believer', auteur='Imagine Dragons'):
+    # Récupérer la boîte correspondante
+    boite = get_object_or_404(Box, url_box=url)
+
+    # Vérifier si la chanson existe déjà
+    try:
+        chanson = Song.objects.filter(name_song=nom_chanson, name_artist=auteur).get()
+        id_chanson = chanson.id_song
+        chanson.n_deposits += 1
+        chanson.save()
+    except:
+        # Créer une nouvelle chanson
+        nouvelle_chanson = Song(name_song=nom_chanson, name_artist=auteur, n_deposits=1)
+        nouvelle_chanson.save()
+        id_chanson = nouvelle_chanson.id_song
+
+    # Créer un nouveau dépôt de musique
+    nouveau_depot = Deposit(id_song=id_chanson, id_boite=boite.id_boite, user=request.user.username)
+    nouveau_depot.save()
+
+    # Rediriger vers la page de détails de la boîte
+    return redirect('../', url_boite=url)
