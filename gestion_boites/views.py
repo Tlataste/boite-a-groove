01942d2d7ebd62701d768ca71c4cec7a3d80e7cd
LOURDEL <emilie.lourdel@orange.fr>
2023-05-16 10:36:55 +0200

    affichage des dernieres musiques deposees

--- gestion_boites/views.py
+++ gestion_boites/views.py
@@ -3,13 +3,19 @@ from django.http import HttpResponse
 from rest_framework import generics
 
 from .serializers import BoxSerializer
-from .models import Box
+from .models import Box, Deposit, Song
 
 
 # Create your views here.
 def boite_detail(request, url):
     boite = get_object_or_404(Box, url_box=url)
-    return render(request, 'frontend/affichage_boite.html', {'boite': boite})
+    # Récupérer les deux derniers dépôts liés à la boîte
+    derniers_depots = Deposit.objects.filter(id_boite=boite.id_boite).order_by('-deposited_at')[:2]
+    # Récupérer les noms des chansons correspondantes aux dépôts
+    chansons = Song.objects.filter(id_song__in=derniers_depots.values('id_song'))
+    print(derniers_depots)
+    print(chansons)
+    return render(request, 'frontend/affichage_boite.html', {'boite': boite, 'chansons': chansons})
 
 # def boite_detail(request, url):
 #     boite = get_object_or_404(Box, url_box=url)
