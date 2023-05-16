2b1475ab53723bce177d173c30675d0db77a0d07
Matteofirrone <firrone.matteo@gmail.com>
2023-05-16 15:31:36 +0200

    implemented debouncing

--- frontend/src/components/LiveSearch.js
+++ frontend/src/components/LiveSearch.js
@@ -5,42 +5,43 @@ import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
 
 export default function LiveSearch() {
+  const [searchValue, setSearchValue] = useState("");
   const [jsonResults, setJsonResults] = useState([]);
 
-  // Only run once thansk to []
   useEffect(() => {
-    fetch("https://www.balldontlie.io/api/v1/players") // Select the API to fetch
-      .then((response) => response.json())
-      .then((json) => setJsonResults(json.data)); // Set the result in jsonResults
-  }, []);
-  console.log(jsonResults);
+    const delayDebounceFn = setTimeout(() => {
+      fetch(`https://www.balldontlie.io/api/v1/players?search=${searchValue}`)
+        .then((response) => response.json())
+        .then((json) => setJsonResults(json.data));
+    }, 2000);
+    console.log(jsonResults);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchValue]);
 
   return (
     <Stack sx={{ width: 300, margin: "auto" }}>
       <Autocomplete
         freeSolo
-        options={jsonResults} // Options for the autocomplete = content
-        getOptionLabel={(
-          jsonResults // Defines how to extract the label for each option
-        ) => `${jsonResults.first_name} ${jsonResults.last_name}`}
-        sx={{ width: 300 }} // Styling
-        isOptionEqualToValue={(
-          // Compares the option with the selected value.
-          option,
-          value
-        ) => option.first_name === value.first_name}
-        noOptionsText={"No songs availaible"}
-        renderOption={(
-          props,
-          jsonResults // Renders each option
-        ) => (
-          <Box component="li" {...props} key={jsonResults.id}>
-            {jsonResults.first_name} {jsonResults.last_name}
+        options={jsonResults}
+        getOptionLabel={(option) => `${option.first_name} ${option.last_name}`}
+        sx={{ width: 300 }}
+        isOptionEqualToValue={(option, value) =>
+          option.first_name === value.first_name
+        }
+        noOptionsText={"No songs available"}
+        renderOption={(props, option) => (
+          <Box component="li" {...props} key={option.id}>
+            {option.first_name} {option.last_name}
           </Box>
         )}
-        renderInput={(
-          params // Renders the input field for the autocomplete
-        ) => <TextField {...params} label="Search for a song" />}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search for a player"
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        )}
       />
     </Stack>
   );
