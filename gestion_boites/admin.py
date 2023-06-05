cbcb75ab8a5201cc7af3fbcf4f6e0363c6748836
Matteofirrone <firrone.matteo@gmail.com>
2023-06-05 17:41:11 +0200

    fixed app box management

--- gestion_boites/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Box, Deposit, Song
-
-admin.site.site_header = "Administration de la Boîte à Son"
-admin.site.register(Box)
-admin.site.register(Deposit)
-admin.site.register(Song)
