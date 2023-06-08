e070611aadea3e3d44eac11fac4bcef50bf1411b
Matteofirrone <firrone.matteo@gmail.com>
2023-06-08 11:03:31 +0200

    Authentication working

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -31,38 +32,70 @@ function Copyright(props) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
+export default function LoginPage() {
+  // States & Variables
+  const [authenticationSuccess, setAuthenticationSuccess] = useState(false);
+  const [errorMessages, setErrorMessages] = useState("");
 
-const defaultTheme = createTheme();
+  // Methods
+  const sendAndProcessData = async (form) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: form,
+    };
+    try {
+      const response = await fetch("/users/login_user", requestOptions);
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        setAuthenticationSuccess(true);
+        setErrorMessages("");
+      } else {
+        if (response.status === 401) {
+          setErrorMessages("Informations d'identification non valides");
+        } else {
+          setErrorMessages("Vous êtes déjà connecté");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-export default function SignIn() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
+    const jsonData = JSON.stringify({
+      username: data.get("username"),
       password: data.get("password"),
     });
+    console.log(jsonData);
+    sendAndProcessData(jsonData);
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Se connecter
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Se connecter
+        </Typography>
+        {authenticationSuccess ? (
+          <Typography variant="body2" color="text.primary" align="center">
+            Vous vous êtes connecté avec succès!
           </Typography>
+        ) : (
           <Box
             component="form"
             onSubmit={handleSubmit}
@@ -101,6 +134,9 @@ export default function SignIn() {
             >
               Se connecter
             </Button>
+            <Typography variant="body2" color="error" align="center">
+              {errorMessages}
+            </Typography>
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
@@ -114,9 +150,9 @@ export default function SignIn() {
               </Grid>
             </Grid>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+        )}
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   );
 }
