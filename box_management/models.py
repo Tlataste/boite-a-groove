e28ac3b98644d9de6e6a10062002d90a0ccdab04
TUCHAGUES <axel.tuchagues@orange.fr>
2023-06-09 14:54:43 +0200

    Changement sur le modèle pour un affichage propre dans la page d'administration

--- box_management/models.py
+++ box_management/models.py
@@ -13,6 +13,9 @@ class Box(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     client_name = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Song(models.Model):
     title = models.CharField(max_length=50)
@@ -23,9 +26,15 @@ class Song(models.Model):
     platform_id = models.IntegerField(default=0)
     n_deposits = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.title + ' - ' + self.artist
+
 
 class Deposit(models.Model):
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
     box_id = models.ForeignKey(Box, on_delete=models.CASCADE)
     # user_id = models.IntegerField()
     deposited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.song_id) + ' - ' + str(self.box_id)
