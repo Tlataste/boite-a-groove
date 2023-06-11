d2970987d92cdf24b8f2ac13e11a99a57113787f
Anthony De Sousa <hds.anthony@gmail.com>
2023-06-12 01:12:44 +0200

    auto-deletion of profile pic in DB when a user is deleted

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.dispatch import receiver
+from utils import generate_unique_filename
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+
+    def profile_picture_path(instance, filename):
+        # Modify the file name to ensure uniqueness
+        filename = generate_unique_filename(instance, filename)
+        return f"profile_pictures/{filename}"
+
+    profile_picture = models.ImageField(upload_to=profile_picture_path, blank=True)
+
+
+@receiver(models.signals.pre_delete, sender=CustomUser)
+# When a user is deleted, his profile picture is deleted from the database
+def delete_profile_picture(sender, instance, **kwargs):
+    # Delete the profile picture file from storage
+    if instance.profile_picture:
+        instance.profile_picture.delete(False)
