e88a69c162d97adadf01a7c9d27a9c5d3a4e290c
Anthony De Sousa <hds.anthony@gmail.com>
2023-06-12 15:37:21 +0200

    Added profile picture change URL

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register_user', views.RegisterUser.as_view(), name="register"),
     path('check-authentication/', views.CheckAuthentication.as_view(), name='check-authentication'),
     path('change-password', views.ChangePasswordUser.as_view(), name="change-password"),
+    path('change-profile-pic', views.ChangeProfilePicture.as_view(), name="change-profile-pic"),
     path('example', views.example, name="example"),
 ]
