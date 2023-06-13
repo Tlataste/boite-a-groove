f37864ba9d8750be4c528856af25db00262c39ca
Matteofirrone <firrone.matteo@gmail.com>
2023-06-13 09:54:08 +0200

    conditional menu display

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useMemo } from "react";
 import { createRoot } from "react-dom/client";
-import { render } from "react-dom";
 import HomePage from "./HomePage";
 import RegisterPage from "./RegisterPage";
 import LoginPage from "./LoginPage";
@@ -8,7 +7,7 @@ import MusicBox from "./MusicBox/MusicBox";
 import UserProfilePage from "./UserProfilePage";
 import { UserContext } from "./UserContext";
 import MenuAppBar from "./Menu";
-import Box from "@mui/material/Box";
+import { useLocation } from "react-router-dom";
 import { checkUserStatus } from "./UsersUtils";
 
 import {
@@ -50,7 +49,7 @@ export default function App() {
   return (
     <Router>
       <UserContext.Provider value={providerValue}>
-        <MenuAppBar />
+        <DisplayMenu />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route
@@ -76,6 +75,17 @@ export default function App() {
       </UserContext.Provider>
     </Router>
   );
+}
+
+/**
+ * Displays the menu app bar based on the current location.
+ * @returns {JSX.Element|null} The JSX element representing the menu app bar or null.
+ */
+function DisplayMenu() {
+  const location = useLocation();
+  const shouldRenderMenuAppBar = location.pathname !== "/";
+
+  return <>{shouldRenderMenuAppBar && <MenuAppBar />}</>;
 }
 
 const appDiv = document.getElementById("app");
