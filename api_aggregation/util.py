897b3312ae7b8ff54710b7daef1c21fc0dfdc2de
TUCHAGUES <axel.tuchagues@orange.fr>
2023-06-14 11:46:49 +0200

    Modification du code pour l'api aggrégatrice

--- api_aggregation/util.py
+++ api_aggregation/util.py
@@ -1,57 +1,75 @@
 from fuzzywuzzy import fuzz
+from spotify.spotipy_client import sp
+from deezer.util import execute_deezer_api_request
 
 
-def find_matching_song_from_deezer_to_spotify(deezer_song_name, deezer_song_artist, deezer_song_duration, spotify_song_list):
+def find_matching_song(song_name, song_artist, song_duration, song_list):
     best_match = None
     best_match_score = 0
 
-    for spotify_song in spotify_song_list:
-        spotify_song_name = spotify_song['name']
-        spotify_song_artist = spotify_song['artist']
-        spotify_song_duration = spotify_song['duration'] // 1000  # Convertir la durée en secondes
+    for song in song_list:
+        best_song_name = song['name']
+        best_song_artist = song['artist']
+        best_song_duration = song['duration']
 
         # Comparaison des noms de chansons
-        name_match_score = fuzz.ratio(deezer_song_name, spotify_song_name)
+        name_match_score = fuzz.ratio(song_name, best_song_name)
 
         # Comparaison des artistes de chansons
-        artist_match_score = fuzz.ratio(deezer_song_artist, spotify_song_artist)
+        artist_match_score = fuzz.ratio(song_artist, best_song_artist)
 
         # Comparaison des durées de chansons (avec une certaine tolérance)
-        duration_match_score = fuzz.token_sort_ratio(str(deezer_song_duration), str(spotify_song_duration))
+        duration_match_score = fuzz.token_sort_ratio(str(song_duration), str(best_song_duration))
 
         # Calcul d'un score global
         match_score = (name_match_score + artist_match_score + duration_match_score) / 3
 
         if match_score > best_match_score:
             best_match_score = match_score
-            best_match = spotify_song
+            best_match = song
 
     return best_match
 
 
-def find_matching_song_from_spotify_to_deezer(spotify_song_name, spotify_song_artist, spotify_song_duration, deezer_song_list):
-    best_match = None
-    best_match_score = 0
+def search_on_spotify(search_query, song):
+    results = sp.search(q=search_query, type='track')
+    tracks = []
+    # Extract the track data from the results and create a list of tracks
+    for item in results['tracks']['items']:
+        track = {
+            'id': item['id'],
+            'name': item['name'],
+            'artist': item['artists'][0]['name'],
+            'album': item['album']['name'],
+            'image_url': item['album']['images'][0]['url'],
+            'duration': item['duration_ms'] // 1000,
+            'platform_id': 1,
+            'url': item['external_urls']['spotify'],
+        }
+        tracks.append(track)
+    final_song = find_matching_song(song['title'], song['artist'], song['duration'], tracks)
+    return final_song
 
-    for deezer_song in deezer_song_list:
-        deezer_song_name = deezer_song['name']
-        deezer_song_artist = deezer_song['artist']
-        deezer_song_duration = deezer_song['duration']
 
-        # Comparaison des noms de chansons
-        name_match_score = fuzz.ratio(spotify_song_name, deezer_song_name)
-
-        # Comparaison des artistes de chansons
-        artist_match_score = fuzz.ratio(spotify_song_artist, deezer_song_artist)
-
-        # Comparaison des durées de chansons (avec une certaine tolérance)
-        duration_match_score = fuzz.token_sort_ratio(str(spotify_song_duration), str(deezer_song_duration))
-
-        # Calcul d'un score global
-        match_score = (name_match_score + artist_match_score + duration_match_score) / 3
-
-        if match_score > best_match_score:
-            best_match_score = match_score
-            best_match = deezer_song
-
-    return best_match
+def search_on_deezer(search_query, song, session_key):
+    # Search for tracks using the Deezer API
+    results = execute_deezer_api_request(
+        session_key,
+        'search/track?q=' + search_query + '&output=json')
+    results = results.json()
+    tracks = []
+    # Extract the track data from the results and create a list of tracks
+    for item in results['data']:
+        track = {
+            'id': item['id'],
+            'name': item['title'],
+            'artist': item['artist']['name'],
+            'album': item['album']['title'],
+            'image_url': item['album']['cover_medium'],
+            'duration': item['duration'],
+            'platform_id': 2,
+            'url': item['link'],
+        }
+        tracks.append(track)
+    final_song = find_matching_song(song['title'], song['artist'], song['duration'], tracks)
+    return final_song
--- api_aggregation/views.py
+++ api_aggregation/views.py
@@ -14,15 +14,8 @@ class ApiAggregation(APIView):
     def post(self, request):
         # Extract the search query from the request data
         song = request.data.get('song')
-        # song = {
-        #     'name': 'Lose Yourself',
-        #     'artist': 'Eminem',
-        #     'duration': 326,
-        #     'platform_id': 1,
-        # }
 
         # Extract the id of the streaming platform from the request data
-        platform_id = song['platform_id']
         request_platform = request.data.get('platform')
 
         if request_platform == "spotify":
@@ -31,59 +24,22 @@ class ApiAggregation(APIView):
             platform_req_id = 2
 
         try:  # The song already exists in the database
-            final_song = Song.objects.filter(title=song['title'], artist=song['artist'], platform_id=platform_req_id).get()
+            final_song = Song.objects.filter(title=song['title'], artist=song['artist'],
+                                             platform_id=platform_req_id).get()
             return Response(final_song.url, status=status.HTTP_200_OK)
 
         except Song.DoesNotExist:  # The song does not exist in the database
             # Normalize the search query
             search_query = normalize_string(song['title'] + ' ' + song['artist'])
-            tracks = []
 
-            if platform_id == 1 and platform_req_id == 2:  # The streaming platform is Spotify
-                # Search for tracks using the Deezer API
-                results = execute_deezer_api_request(
-                    self.request.session.session_key,
-                    'search/track?q=' + search_query + '&output=json')
-                results = results.json()
-                # Extract the track data from the results and create a list of tracks
-                for item in results['data']:
-                    track = {
-                        'id': item['id'],
-                        'name': item['title'],
-                        'artist': item['artist']['name'],
-                        'album': item['album']['title'],
-                        'image_url': item['album']['cover_medium'],
-                        'duration': item['duration'],
-                        'platform_id': 2,
-                        'url': item['link'],
-                    }
-                    tracks.append(track)
-                final_song = ut.find_matching_song_from_spotify_to_deezer(song['title'], song['artist'],
-                                                                          song['duration'],
-                                                                          tracks)
+            if platform_req_id == 2:  # The streaming platform is Deezer
+                final_song = ut.search_on_deezer(search_query, song, self.request.session.session_key)
                 return Response(final_song['url'], status=status.HTTP_200_OK)
 
-            elif platform_id == 2 and platform_req_id == 1:  # The streaming platform is Deezer
-                # Search for tracks using the Spotipy client
-                results = sp.search(q=search_query, type='track')
-
-                # Extract the track data from the results and create a list of tracks
-                for item in results['tracks']['items']:
-                    track = {
-                        'id': item['id'],
-                        'name': item['name'],
-                        'artist': item['artists'][0]['name'],
-                        'album': item['album']['name'],
-                        'image_url': item['album']['images'][0]['url'],
-                        'duration': item['duration_ms'] // 1000,
-                        'platform_id': 1,
-                        'url': item['external_urls']['spotify'],
-                    }
-                    tracks.append(track)
-                    final_song = ut.find_matching_song_from_deezer_to_spotify(song['title'], song['artist'],
-                                                                              song['duration'], tracks)
-
+            elif platform_req_id == 1:  # The streaming platform is Spotify
+                final_song = ut.search_on_spotify(search_query, song)
                 return Response(final_song['url'], status=status.HTTP_200_OK)
+
             else:
                 # Return an error response
                 return Response({'error': 'Invalid platform ID.'}, status=status.HTTP_400_BAD_REQUEST)
