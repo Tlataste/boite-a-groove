a8399adc40a12120ba6e4a585c0c8336ee8a64b0
Matteofirrone <firrone.matteo@gmail.com>
2023-06-16 15:12:45 +0200

    code refactoring

--- frontend/src/components/RedirectToMobile.js
+++ frontend/src/components/RedirectToMobile.js
@@ -1,17 +1,8 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserContext } from "./UserContext";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import Avatar from "@mui/material/Avatar";
-import PersonIcon from "@mui/icons-material/Person";
 import Button from "@mui/material/Button";
-import AlbumIcon from "@mui/icons-material/Album";
 
 export default function RedirectToMobile() {
   // States & Variables
