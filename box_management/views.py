4134a761733a11d5f4940a7ba3dcd3c24eaffa6c
Anthony De Sousa <hds.anthony@gmail.com>
2023-06-19 18:34:49 +0200

    Handled anonymous user deposit

--- box_management/views.py
+++ box_management/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.contrib.auth.models import AnonymousUser
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView  # Generic API view
@@ -68,9 +68,11 @@ class GetBox(APIView):
                         platform_id=song_platform_id, n_deposits=1)
             song.save()
 
+
         # Create a new deposit
         box = Box.objects.filter(name=box_name).get()
-        new_deposit = Deposit(song_id=song, box_id=box)
+        user = request.user if not isinstance(request.user, AnonymousUser) else None
+        new_deposit = Deposit(song_id=song, box_id=box, user=user)
         new_deposit.save()
         new_deposit = DepositSerializer(new_deposit).data
         # Rediriger vers la page de détails de la boîte
