c986bc0440e8f5e34a31543a650532de7620d39c
Matteofirrone <firrone.matteo@gmail.com>
2023-06-21 10:05:39 +0200

    prepared frontend with notes

---
+++ frontend/src/components/MusicBox/OnBoarding/IncentiveNote.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect, useContext } from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+
+// Styles
+const styles = {
+  button: {
+    borderRadius: "20px",
+    backgroundImage: "linear-gradient(to right, #fa9500, #fa4000)",
+    color: "white",
+    border: "none",
+    textTransform: "none",
+    "&:hover": {
+      border: "none",
+    },
+  },
+};
+
+export default function IncentiveNote({ setStage, searchSong }) {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  function handleAddNoteButtonClick(note) {
+    setStage(4);
+  }
+
+  function Item(note, sentence) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "10px",
+        }}
+      >
+        <Typography variant="subtitle1">{sentence}</Typography>
+        <Button
+          style={styles.button}
+          variant="contained"
+          onClick={() => handleAddNoteButtonClick(note)}
+        >
+          Ajouter
+        </Button>
+      </Box>
+    );
+  }
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "10px",
+      }}
+    >
+      <Typography variant="h6">Ajoute une note d'incitation</Typography>
+      <Box
+        sx={{
+          width: "90%",
+          typography: "body1",
+          backgroundColor: "white",
+          margin: "auto",
+          borderRadius: "20px",
+        }}
+      >
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Moods" value="1" />
+              <Tab label="Quelqu'un" value="2" />
+              <Tab label="Période de vie" value="3" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">
+            {Item("calme", "Cette chanson m'apaise et me détend !")}
+          </TabPanel>
+          <TabPanel value="1">
+            {Item("danse", "Cette chanson me donne envie de danser !")}
+          </TabPanel>
+          <TabPanel value="1">
+            {Item("inspire", "Cette chanson me pousse à être créatif !")}
+          </TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+          <TabPanel value="3">Item Three</TabPanel>
+        </TabContext>
+      </Box>
+      <Button
+        style={styles.button}
+        variant="contained"
+        onClick={() => setStage(4)}
+      >
+        Continuer sans note
+      </Button>
+    </Box>
+  );
+}
