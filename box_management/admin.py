6e442647e165faa6da9a11564dbb9c7069d9bde9
LOURDEL <emilie.lourdel@orange.fr>
2023-06-21 12:12:18 +0200

    correction bugs

--- box_management/admin.py
+++ box_management/admin.py
@@ -6,6 +6,7 @@ from django.db.models.functions import TruncMonth, TruncWeek, TruncDay
 from django.http import HttpResponse
 from import_export.admin import ImportExportModelAdmin
 from .models import *
+from users.models import CustomUser
 
 
 class BoxAdmin(ImportExportModelAdmin, admin.ModelAdmin):
@@ -22,84 +23,96 @@ class DepositAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ('song_id__title', 'box_id__name', 'user')
     ordering = ('-deposited_at',)
 
-    def export_deposits_csv(self, request, queryset):
+    def export_deposits_global(self, request, queryset):
         response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="deposits.csv"'
+        response['Content-Disposition'] = 'attachment; filename="deposits_global.csv"'
 
         writer = csv.writer(response)
-        writer.writerow(['Month', 'Week', 'Day', 'Total Deposits'])
+        writer.writerow(['Box', 'Period', 'Number of Deposits'])
 
-        # Perform database queries to get deposit counts by month, week, and day
-        deposits_by_month = Deposit.objects.values('deposited_at__month').annotate(total=Count('id')).values(
-            'deposited_at__month', 'total')
-        deposits_by_week = Deposit.objects.values('deposited_at__week').annotate(total=Count('id')).values(
-            'deposited_at__week', 'total')
-        deposits_by_day = Deposit.objects.values('deposited_at__date').annotate(total=Count('id')).values(
-            'deposited_at__date', 'total')
-        total_deposits = Deposit.objects.count()
+        # Export by box and month
+        deposits_month = Deposit.objects.values('box_id__name').annotate(period=TruncMonth('deposited_at')).values(
+            'box_id__name', 'period').annotate(count=Count('id'))
+        for deposit in deposits_month:
+            writer.writerow([deposit['box_id__name'], deposit['period'].strftime('%Y-%m'), deposit['count']])
 
-        # Write data to CSV
-        for month in deposits_by_month:
-            writer.writerow([month['deposited_at__month'], '', '', month['total']])
-        for week in deposits_by_week:
-            writer.writerow(['', week['deposited_at__week'], '', week['total']])
-        for day in deposits_by_day:
-            writer.writerow(['', '', day['deposited_at__date'], day['total']])
-        writer.writerow(['', '', '', total_deposits])
+        # Export by box and week
+        deposits_week = Deposit.objects.values('box_id__name').annotate(period=TruncWeek('deposited_at')).values(
+            'box_id__name', 'period').annotate(count=Count('id'))
+        for deposit in deposits_week:
+            writer.writerow([deposit['box_id__name'], deposit['period'].strftime('%Y-%W'), deposit['count']])
+
+        # Export by box and day
+        deposits_day = Deposit.objects.values('box_id__name').annotate(period=TruncDay('deposited_at')).values(
+            'box_id__name', 'period').annotate(count=Count('id'))
+        for deposit in deposits_day:
+            writer.writerow([deposit['box_id__name'], deposit['period'].strftime('%Y-%m-%d'), deposit['count']])
 
         return response
 
-    export_deposits_csv.short_description = "Export deposits as CSV"
+    export_deposits_global.short_description = "Export deposits as CSV"
 
-    def export_deposits_by_week_and_day_csv(self, request, queryset):
+    def export_deposits_distribution(self, request, queryset):
         response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="deposits_by_week_and_day.csv"'
+        response['Content-Disposition'] = 'attachment; filename="deposits_distribution.csv"'
 
         writer = csv.writer(response)
-        writer.writerow(['Week', 'Day', 'Total Deposits'])
+        writer.writerow(['Box', 'Week', 'Day', 'Number of Deposits'])
 
-        # Perform database queries to get deposit counts by week and day
-        deposits_by_week = Deposit.objects.annotate(week=TruncWeek('deposited_at')).values('week').annotate(
-            total=Count('id')).values('week', 'total')
-        deposits_by_day = Deposit.objects.annotate(day=TruncDay('deposited_at')).values('day').annotate(
-            total=Count('id')).values('day', 'total')
-
-        # Write data to CSV
-        for week in deposits_by_week:
-            writer.writerow([week['week'].strftime('%U, %Y'), '', week['total']])
-        for day in deposits_by_day:
-            writer.writerow(['', day['day'].strftime('%Y-%m-%d'), day['total']])
+        # Export by box, week and day
+        deposits = Deposit.objects.values('box_id__name').annotate(week=TruncWeek('deposited_at'),
+                                                                   day=TruncDay('deposited_at')).values('box_id__name',
+                                                                                                        'week',
+                                                                                                        'day').annotate(
+            count=Count('id'))
+        for deposit in deposits:
+            writer.writerow(
+                [deposit['box_id__name'], deposit['week'].strftime('%Y-%W'), deposit['day'].strftime('%Y-%m-%d'),
+                 deposit['count']])
 
         return response
 
-    export_deposits_by_week_and_day_csv.short_description = "Export deposits by week and day as CSV"
+    export_deposits_distribution.short_description = "Export deposits distribution as CSV"
 
     def export_active_users_csv(self, request, queryset):
-        import csv
-        from django.http import HttpResponse
-
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="active_users.csv"'
 
         writer = csv.writer(response)
-        writer.writerow(['User', 'Month', 'Week', 'Total Deposits'])
+        writer.writerow(['User', 'Box', 'Period', 'Number of Deposits'])
 
-        # Perform your database queries and generate the data for CSV export
-        # Replace the following code with your own logic
-        active_users_by_month = Deposit.objects.filter().count()
-        active_users_by_week = Deposit.objects.filter().count()
-        total_deposits_by_user = Deposit.objects.count()
+        # Export active users globally by month
+        active_users_month = CustomUser.objects.filter(deposit__isnull=False).annotate(
+            period=TruncMonth('deposit__deposited_at')).values('username', 'period').annotate(
+            count=Count('deposit__id'))
+        for user in active_users_month:
+            writer.writerow([user['username'], 'Global', user['period'].strftime('%Y-%m'), user['count']])
 
-        writer.writerow(['', '', '', active_users_by_month])
-        writer.writerow(['', '', '', active_users_by_week])
-        writer.writerow(['', '', '', total_deposits_by_user])
+        # Export active users globally by week
+        active_users_week = CustomUser.objects.filter(deposit__isnull=False).annotate(
+            period=TruncWeek('deposit__deposited_at')).values('username', 'period').annotate(count=Count('deposit__id'))
+        for user in active_users_week:
+            writer.writerow([user['username'], 'Global', user['period'].strftime('%Y-%W'), user['count']])
+
+        # Export active users by box and month
+        active_users_box_month = CustomUser.objects.filter(deposit__isnull=False).annotate(
+            period=TruncMonth('deposit__deposited_at')).values('username', 'deposit__box_id__name', 'period').annotate(
+            count=Count('deposit__id'))
+        for user in active_users_box_month:
+            writer.writerow(
+                [user['username'], user['deposit__box_id__name'], user['period'].strftime('%Y-%m'), user['count']])
+
+        # Export active users by box and week
+        active_users_box_week = CustomUser.objects.filter(deposit__isnull=False).annotate(
+            period=TruncWeek('deposit__deposited_at')).values('username', 'deposit__box_id__name', 'period').annotate(
+            count=Count('deposit__id'))
+        for user in active_users_box_week:
+            writer.writerow(
+                [user['username'], user['deposit__box_id__name'], user['period'].strftime('%Y-%W'), user['count']])
 
         return response
 
     export_active_users_csv.short_description = "Export active users as CSV"
-
-    actions = ['export_deposits_csv', 'export_deposits_by_week_and_day_csv', 'export_active_users_csv',
-               'export_popular_songs_csv']
 
     def export_popular_songs_csv(self, request, queryset):
         import csv
@@ -126,6 +139,10 @@ class DepositAdmin(ImportExportModelAdmin, admin.ModelAdmin):
         return response
 
     export_popular_songs_csv.short_description = "Export popular songs as CSV"
+
+    actions = ['export_deposits_global', 'export_deposits_distribution', 'export_active_users_csv',
+               'export_popular_songs_csv']
+
 
 admin.site.site_header = "Administration de la Boîte à Son"
 admin.site.register(Box, BoxAdmin)
