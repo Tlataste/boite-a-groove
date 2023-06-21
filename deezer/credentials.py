8efd940957842ccf2be79a1955020007828a7c99
Matteofirrone <firrone.matteo@gmail.com>
2023-06-21 14:24:20 +0200

    implemented deezer login

--- deezer/credentials.py
+++ deezer/credentials.py
@@ -10,3 +10,14 @@ try:
 except FileNotFoundError as fnfe:
     print(f'\'credential.json\' not found.')
     exit()
+
+try:
+    with open("credential.json") as json_data_file:
+        data = json.load(json_data_file)
+
+        LOGIN_APP_ID = data['Credentials'][2]['app_id']
+        LOGIN_APP_SECRET = data['Credentials'][2]['app_secret']
+        LOGIN_REDIRECT_URI = data['Credentials'][2]['redirect_uri']
+except FileNotFoundError as fnfe:
+    print(f'\'credential.json\' not found.')
+    exit()
