7520d9a066c188536c8fa6d3d8cee1592ea39de5
Matteofirrone <firrone.matteo@gmail.com>
2023-06-21 14:33:31 +0200

    removed api app

--- api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class MusicDeposit(models.Model):
-    host = models.CharField(max_length=15)
-    music_id = models.CharField(max_length=15, null=False)
-    time = models.DateTimeField(auto_now_add=True)
