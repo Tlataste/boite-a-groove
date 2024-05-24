d1d745d42fe4dee8c618ff2792d92753590db9af
Lex <alexandre.dromel@gmail.com>
2024-05-24 13:16:50 +0200

    6-grid on BoxStartup

--- frontend/src/components/MusicBox/OnBoarding/BoxStartup.js
+++ frontend/src/components/MusicBox/OnBoarding/BoxStartup.js
@@ -12,8 +12,16 @@ export default function BoxStartup({ setStage, boxInfo, className }) {
           className={className}
           elevation={3}
         >
-          <div className="decoration">
+          <div className="decoration" style={{ paddingTop: '30px', paddingLeft: '10px', paddingRight: '10px', boxSizing: 'border-box' }}>
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, minmax(0, 1fr))', gap: '10px', width: '100%', overflow: 'hidden' }}>
+              {boxInfo.box.last_deposits.map((deposit, index) => (
+                <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`} >
+                  <img src={deposit.image_url} alt={`Slide ${index + 1}`} style={{ width: '100%', height: 'auto' }} />
+                </div>
+              ))}
+            </div>
           </div>
+
           <div className="bottom-content">
             <div className="bottom-content__wrapper">
               <button className="btn-secondary">
@@ -21,9 +29,9 @@ export default function BoxStartup({ setStage, boxInfo, className }) {
                   {boxInfo.box.name}
                 </span>
               </button>
-              
+
               <h1>{boxInfo.deposit_count} chansons sur cet arrêt, échanges-en une pour la découvrir</h1>
-        
+
 
               <button
                 className="btn-primary"
