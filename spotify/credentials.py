1a9c6b0becd91461fd7427f75b499317e77268d7
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-08-10 18:58:23 +0200

    Update credentials.py

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -6,6 +6,7 @@ print("spotify/credentials.py Looking for JSON in:", os.getcwd())
 try:
     with open("credential.json") as json_data_file:
         data = json.load(json_data_file)
+        print(data)
 
         CLIENT_ID = data['Credentials'][0]['client_id']
         CLIENT_SECRET = data['Credentials'][0]['client_secret']
@@ -17,7 +18,9 @@ except FileNotFoundError as fnfe:
 
 except json.JSONDecodeError:
     print("'credential.json' is invalid or empty. (in spotify/credential.py)")
+    
     exit()
+
 
 
 
