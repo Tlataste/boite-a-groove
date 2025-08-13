11f7e5a0c1c570596ec8fdde2968158ddec71e9b
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-08-14 00:13:24 +0200

    Update gulpfile.js

--- frontend/assets/gulpfile.js
+++ frontend/assets/gulpfile.js
@@ -80,27 +80,15 @@ let tools = {
                 return $.if((sassLintEnabled && _config.lint.failOnError), $.sassLint.failOnError())
             });
     },
-    cssCompilerPipelineBuilder(compiler, sourcemap, minify) {
-        return lazypipe()
-            .pipe(() => {
-                return $.if(sourcemap, $.sourcemaps.init())
-            })
-            .pipe(() => {
-                return $.if((compiler === 'less'), $.less());
-            })
-            // >>> Remplacement: utiliser gulpSass (dart-sass) au lieu de $.sass/node-sass
-            .pipe(() => {
-                return $.if((compiler === 'sass'), gulpSass({ /* outputStyle géré par cleanCss ensuite */ }));
-            })
-            .pipe(() => {
-                return autoprefixer()
-            })
-            .pipe(() => {
-                return $.if(minify, $.cleanCss())
-            })
-            .pipe(() => {
-                return $.if(sourcemap, $.sourcemaps.write('.'))
-            });
+    function cssCompilerPipelineBuilder(compiler, sourcemap, minify) {
+      return lazypipe()
+        .pipe(() => $.if(sourcemap, $.sourcemaps.init()))
+        .pipe(() => $.if((compiler === 'less'), $.less()))
+        .pipe(() => $.if((compiler === 'sass'), gulpSass({})))
+        .pipe(() => autoprefixer())
+        .pipe(() => $.if(minify, $.cleanCss()))
+        .pipe(() => $.if(sourcemap, $.sourcemaps.write('.')));
+    }
     },
     printTaskState(project, type) {
         return through2({ objectMode: true }, function (chunk, enc, callback) {
